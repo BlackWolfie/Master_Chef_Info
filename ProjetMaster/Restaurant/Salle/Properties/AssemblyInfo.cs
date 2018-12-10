1afebfccb8b6bd88104440a9acc2a8b9f45eeb93
Luc <Luc@MSI>
2018-12-10 14:29:18 +0100

    Revert "modif"
    This reverts commit f9777bf18abd81b961c7f5ae12502754743c8ff9.

--- ProjetMaster/Restaurant/Salle/Properties/AssemblyInfo.cs
+++ ProjetMaster/Restaurant/Salle/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("e04b9ab6-a2e1-4a9c-84be-16145eec4011")]
+[assembly: Guid("e62f7765-0c9e-44f4-8676-09564f6fe660")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
