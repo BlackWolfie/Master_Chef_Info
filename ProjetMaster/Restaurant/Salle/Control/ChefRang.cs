1977faa9f7473d29d5d0ffc9168731ec9bf1b97d
Luc <Luc@MSI>
2018-12-10 16:55:56 +0100

    socket

--- ProjetMaster/Restaurant/Salle/Control/ChefRang.cs
+++ ProjetMaster/Restaurant/Salle/Control/ChefRang.cs
@@ -2,12 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Salle.Control
 {
     class ChefRang
     {
+        private static void Envoi(message message)
+        {
+            //Sérialisation du message en tableau de bytes.
+            byte[] msge = Encoding.Default.GetBytes(message.e);
+            byte[] msgp = Encoding.Default.GetBytes(message.p);
+            byte[] msgd = Encoding.Default.GetBytes(message.d);
+            byte[] msgn = Encoding.Default.GetBytes(message.N);
+
+
+            UdpClient udpClient = new UdpClient();
+
+            //La méthode Send envoie un message UDP.
+            udpClient.Send(msge, msge.Length, "127.0.0.1", 5035);
+            udpClient.Send(msgp, msgp.Length, "127.0.0.1", 5035);
+            udpClient.Send(msgd, msgd.Length, "127.0.0.1", 5035);
+            udpClient.Send(msgn, msgn.Length, "127.0.0.1", 5035);
+
+            udpClient.Close();
+        }
         public ChefRang()
         {
 
