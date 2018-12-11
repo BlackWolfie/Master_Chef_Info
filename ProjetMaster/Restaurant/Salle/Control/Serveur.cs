f993aab7d6a1e8da51f7018ad709dbc7f35d67d7
Luc <Luc@MSI>
2018-12-11 16:15:45 +0100

    reception salle

--- ProjetMaster/Restaurant/Salle/Control/Serveur.cs
+++ ProjetMaster/Restaurant/Salle/Control/Serveur.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Salle.Control
 {
     class Serveur
     {
+        private static Thread thEcoute;
+
         public Serveur()
         {
 
@@ -31,6 +36,42 @@ namespace Salle.Control
         public void ramassage()
         {
 
+        }
+        public void Thread()
+        {
+            //Préparation et démarrage du thread en charge d'écouter.
+            thEcoute = new Thread(new ThreadStart(Ecouter));
+            thEcoute.Start();
+        }
+        private static void Ecouter()
+        {
+            //Console.WriteLine("Préparation à l'écoute...");
+
+            //On crée le serveur en lui spécifiant le port sur lequel il devra écouter.
+            UdpClient serveur = new UdpClient(5036);
+            //creation liste de commande par table
+            List<string> commande = new List<string>();
+            string[] listage = new string[4];
+
+            //Création d'une boucle infinie qui aura pour tâche d'écouter.
+            while (true)
+            {
+                //Création d'un objet IPEndPoint qui recevra les données du Socket distant.
+                IPEndPoint client = null;
+
+                //On écoute jusqu'à recevoir un message.
+                byte[] data = serveur.Receive(ref client);
+
+                //Décryptage et affichage du message.
+                string message = Encoding.Default.GetString(data);
+
+                commande.Add(message);
+
+                foreach (string command in commande)
+                {
+                     listage =  command.Split(new char[] { ',' });
+                }
+            }
         }
     }
 
